9e658f5116b31a854e357892135060923209e438
Somya Sarathi Samal <soms9edu@gmail.com>
2022-05-10 17:54:45 +0530

    Added python code for infix to prefix

---
+++ 07_infix_to_prefix.py
@@ -0,0 +1,63 @@
+OPERATORS = set(['+', '-', '*', '/', '(', ')'])
+
+PRI = {'+': 1, '-': 1, '*': 2, '/': 2}
+
+def infix_to_prefix(formula):
+    op_stack = []
+
+    exp_stack = []
+
+    for ch in formula:
+
+        if not ch in OPERATORS:
+
+            exp_stack.append(ch)
+
+        elif ch == '(':
+
+            op_stack.append(ch)
+
+        elif ch == ')':
+
+            while op_stack[-1] != '(':
+                op = op_stack.pop()
+
+                a = exp_stack.pop()
+
+                b = exp_stack.pop()
+
+                exp_stack.append(op + b + a)
+
+            op_stack.pop()  # pop '('
+
+        else:
+
+            while op_stack and op_stack[-1] != '(' and PRI[ch] <= PRI[op_stack[-1]]:
+                op = op_stack.pop()
+
+                a = exp_stack.pop()
+
+                b = exp_stack.pop()
+
+                exp_stack.append(op + b + a)
+
+            op_stack.append(ch)
+
+            # leftover
+
+    while op_stack:
+        op = op_stack.pop()
+
+        a = exp_stack.pop()
+
+        b = exp_stack.pop()
+
+        exp_stack.append(op + b + a)
+
+    print(f'PREFIX: {exp_stack[-1]}')
+
+    return exp_stack[-1]
+
+expres = input("INPUT THE EXPRESSION: ")
+
+pre = infix_to_prefix(expres)
